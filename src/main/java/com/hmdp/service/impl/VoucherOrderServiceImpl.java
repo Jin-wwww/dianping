51173e40e7b61af16ebbc3872c9c2dc52273250e
jin wenyong <jwyzszh8@gmail.com>
2026-05-01 19:47:33 +0800

    实现基本的优惠券下单功能

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +25,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券信息
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        //判断秒杀是否开始
+        LocalDateTime beginTime = voucher.getBeginTime();
+        if (beginTime.isAfter(LocalDateTime.now())) {
+            //尚未开始
+            return Result.fail("秒杀尚未开始！");
+        }
+        //判断秒杀是否已经结束
+        LocalDateTime endTime = voucher.getEndTime();
+        if (endTime.isBefore(LocalDateTime.now())) {
+            //已经结束
+            return Result.fail("秒杀已经结束！");
+        }
+        //判断库存是否充足
+        if (voucher.getStock()<1) {
+            return Result.fail("库存不足");
+        }
+        //扣减库存
+        seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId).update();
+        //创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        //订单id:使用全局唯一Id
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        //用户id
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+        //优惠券id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        //返回订单id
+        return Result.ok(orderId);
+    }
 }
